f1ee71a60e628810d07d6986eeeb701e81c55c81
Thais Kusuki <thais.kusuki@gmail.com>
2018-02-14 22:39:22 -0200

    Listing all articles and adding links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,18 @@
 class ArticlesController < ApplicationController
   def new
   end
+
   def create
     @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
   end
+
+  def index
+  @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
